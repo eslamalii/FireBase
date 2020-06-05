bffd0dda128b0abe6840d69182ae603c6c95b74c
eslamlii <preslamali@hotmail.com>
2020-06-05 05:57:41 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("message");
+
+        databaseReference.setValue("Hello Firebase");
+    }
+}
